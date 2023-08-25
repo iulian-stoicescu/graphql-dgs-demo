e10605f00564435f60fe5795c920c4053580c23a
iulian-stoicescu <iulianstoicescu16@gmail.com>
2023-08-25 21:49:24 +0300

    initial structure to test the app, start it and go to http://localhost:8080/graphiql run this query: {     shows {         title         releaseYear     } }

---
+++ src/main/java/com/example/graphqldgsdemo/GraphqlDgsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqldgsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDgsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDgsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graphqldgsdemo/GraphqlDgsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqldgsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDgsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
