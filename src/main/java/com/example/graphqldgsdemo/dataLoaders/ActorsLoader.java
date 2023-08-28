78b5b1fd7667381a4da677d4d118d149e443bfa0
iulian-stoicescu <iulianstoicescu16@gmail.com>
2023-08-28 23:18:13 +0300

    Currently, if you make a request to get all the shows with all the data you will see the `inside ActorsFetcher` log 5 times and the `inside ActorsLoader` log once. The `ActorsLoader` will wait for all the show ids (5) that are coming from the `ActorFetcher` before calling the `ActorService` to get their actors

--- src/main/java/com/example/graphqldgsdemo/dataLoaders/ActorsLoader.java
+++ src/main/java/com/example/graphqldgsdemo/dataLoaders/ActorsLoader.java
@@ -4,6 +4,7 @@ import com.example.graphqldgsdemo.DgsConstants;
 import com.example.graphqldgsdemo.services.ActorsService;
 import com.example.graphqldgsdemo.types.Actor;
 import com.netflix.graphql.dgs.DgsDataLoader;
+import lombok.extern.slf4j.Slf4j;
 import org.dataloader.BatchLoaderEnvironment;
 import org.dataloader.MappedBatchLoaderWithContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+@Slf4j
 @DgsDataLoader(name = DgsConstants.SHOW.Actors)
 public class ActorsLoader implements MappedBatchLoaderWithContext<String, List<Actor>> {
 
@@ -26,6 +28,7 @@ public class ActorsLoader implements MappedBatchLoaderWithContext<String, List<A
 
     @Override
     public CompletionStage<Map<String, List<Actor>>> load(Set<String> keys, BatchLoaderEnvironment environment) {
+        log.info("inside ActorsLoader");
         return CompletableFuture.supplyAsync(() -> this.actorsService.getActorsByShowIds(new ArrayList<>(keys)));
     }
 }
